2a6e08d10f4286441832b502ef818a22342947f9
Juan Aristizabal <spjhon@gmail.com>
2023-05-08 15:58:29 -0500

    Implementacion de la segunda capa del json

--- Leccion 003 Selectors_Especividad/script.js
+++ Leccion 003 Selectors_Especividad/script.js
@@ -13,31 +13,44 @@ function readTextFile(file, callback) {
 //usage:
 readTextFile("./content.json", function(text){
   const Selectors = JSON.parse(text);
-  const mainContainer = document.getElementsByClassName("main_container");
-  Selectors.Selectors.map((selector) => {
-    
-    mainContainer[0].innerHTML += `<script async src="https://cpwebassets.codepen.io/assets/embed/ei.js"></script>
-    <div class="modal fade" id=${selector.id} tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-xl">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h1 class="modal-title fs-5" id="exampleModalLabel">${selector.titulo}</h1>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-      
-            ${selector.parrafo}
-            ${selector.codepen}
-            
+  const hiddenMainContainer = document.getElementsByClassName("hidden_main_container");
 
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+  
+
+  Selectors.Selectors_Specificity.map((subject) => {
+    
+    /*aqui codigo del tema*/
+    console.log(subject.topic);
+  const mainContainer = document.getElementsByClassName("hidden_main_container");
+    
+    mainContainer[0].innerHTML += `<hr/><hr/>${subject.topic}`;
+
+
+
+    subject.subject.map((selector) => {
+      hiddenMainContainer[0].innerHTML += `<script async src="https://cpwebassets.codepen.io/assets/embed/ei.js"></script>
+      <div class="modal fade" id=${selector.id} tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-xl">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">${selector.titulo}</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+        
+              ${selector.parrafo}
+              ${selector.codepen}
+              
+  
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    `;
+      `;
+    });
   });
 });
 
