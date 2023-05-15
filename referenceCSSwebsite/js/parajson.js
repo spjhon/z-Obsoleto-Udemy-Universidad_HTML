5259612acefbda482f196cdaef36bc2c1721d036
Juan Aristizabal <spjhon@gmail.com>
2023-05-15 18:10:54 -0500

    no mucho, solo trabajo de oficina

--- referenceCSSwebsite/js/parajson.js
+++ referenceCSSwebsite/js/parajson.js
@@ -1,15 +1,15 @@
 const iframeJson = {
   
-    title: "::part()",
-    description: "The ::placeholder CSS pseudo-element represents the placeholder text in an input> or textarea> element.",
+    title: "::selection",
+    description: "The ::selection CSS pseudo-element applies styles to the part of a document that has been highlighted by the user (such as clicking and dragging the mouse across text).",
     subject: [
       {
         id: "007",
-        titulo: "Ejemplo ::placeholder",
-        parrafo: "Ejemplo de un ::placeholder",
-        codepen: `<iframe height="300" style="width: 100%;" scrolling="no" title="Pseudo-Elemento ::placeholder" src="https://codepen.io/spjhon/embed/LYgOOVw?default-tab=html%2Cresult" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true">
-        See the Pen <a href="https://codepen.io/spjhon/pen/LYgOOVw">
-        Pseudo-Elemento ::placeholder</a> by Juan Camilo (<a href="https://codepen.io/spjhon">@spjhon</a>)
+        titulo: "Ejemplo ::selection",
+        parrafo: "Ejemplo de un ::selection",
+        codepen: `<iframe height="300" style="width: 100%;" scrolling="no" title="Pseudo-Element ::selection" src="https://codepen.io/spjhon/embed/KKGyyVQ?default-tab=html%2Cresult" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true">
+        See the Pen <a href="https://codepen.io/spjhon/pen/KKGyyVQ">
+        Pseudo-Element ::selection</a> by Juan Camilo (<a href="https://codepen.io/spjhon">@spjhon</a>)
         on <a href="https://codepen.io">CodePen</a>.
       </iframe>`
       }
