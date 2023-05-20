0792af54a6afd6cbe2a54571e7975adbd4ba5a1a
Juan Aristizabal <spjhon@gmail.com>
2023-05-20 15:02:42 -0500

    adelanto json

--- referenceCSSwebsite/js/parajson.js
+++ referenceCSSwebsite/js/parajson.js
@@ -1,20 +1,19 @@
 const iframeJson = {
   
-    title: "::selection",
-    description: "The ::selection CSS pseudo-element applies styles to the part of a document that has been highlighted by the user (such as clicking and dragging the mouse across text).",
+    title: ":any-link",
+    description: "It matches every a or area element that has an href attribute. Thus, it matches all elements that match :link or :visited.",
     subject: [
       {
         id: "007",
-        titulo: "Ejemplo ::selection",
-        parrafo: "Ejemplo de un ::selection",
-        codepen: `<iframe height="300" style="width: 100%;" scrolling="no" title="Pseudo-Element ::selection" src="https://codepen.io/spjhon/embed/KKGyyVQ?default-tab=html%2Cresult" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true">
-        See the Pen <a href="https://codepen.io/spjhon/pen/KKGyyVQ">
-        Pseudo-Element ::selection</a> by Juan Camilo (<a href="https://codepen.io/spjhon">@spjhon</a>)
-        on <a href="https://codepen.io">CodePen</a>.
-      </iframe>`
+        titulo: "Ejemplo :any-link",
+        parrafo: "Ejemplo de un :any-link",
+        codepen: `<iframe height="300" style="width: 100%;" scrolling="no" title="Pseudo-Clase :active" src="https://codepen.io/spjhon/embed/oNaQJee?default-tab=html%2Cresult" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true">
+  See the Pen <a href="https://codepen.io/spjhon/pen/oNaQJee">
+  Pseudo-Clase :active</a> by Juan Camilo (<a href="https://codepen.io/spjhon">@spjhon</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+</iframe>`
       }
     ]
-  
 }
 
 
